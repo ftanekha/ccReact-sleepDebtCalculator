c6f0167e42c6e24555c8683c6c914b0e1a79d554
Farai Tanekha <30661410+codemusltd@users.noreply.github.com>
2021-08-11 15:01:25 +0100

    implement getActualSleepHours() without calling getSleepHours() use literal numbers and the + operator to rewrite getActualSleepHours()

--- sleepdebtCalculator.js
+++ sleepdebtCalculator.js
@@ -1,41 +1,35 @@
 'use strict'
 
-//determine number of hours of sleep you got per night of the week
-function getSleepHours(day){
-  //make the function flexible
-    day = day.toLowerCase()
-    //return the number of hours you slept each night/day
-    switch(day){
-        case 'sunday': case'monday': 
-        case 'tuesday':
-            return 5.5
-        case 'wednesday':
-            return 6
-        case 'thursday':
-            return 7
-        case 'friday': case 'saturday':
-            return 8.5
-        case 'sunday':
-            return 8
-        default: 
-            return 'day unrecongnized!'
-    }
-}
+// //determine number of hours of sleep you got per night of the week
+// function getSleepHours(day){
+//   //make the function flexible
+//     day = day.toLowerCase()
+//     //return the number of hours you slept each night/day
+//     switch(day){
+//         case 'sunday': case'monday': 
+//         case 'tuesday':
+//             return 5.5
+//         case 'wednesday':
+//             return 6
+//         case 'thursday':
+//             return 7
+//         case 'friday': case 'saturday':
+//             return 8.5
+//         case 'sunday':
+//             return 8
+//         default: 
+//             return 'day unrecongnized!'
+//     }
+// }
 
 //get the total sleep hours that you actually slept
-function getActualSleepHours(){
-    let hoursSlept = []
-    const daysOfWeek = [
-        'monday', 'tuesday', 'wednesday', 'thursday',
-        'friday', 'saturday', 'sunday'
-    ]
+function getActualSleepHours(a = 5.5, b = 5.5, c = 5.5, d = 6, e = 7, f = 8.5, g = 8){
+    //check that hours are valid
+    for(let hours of [a,b,c,d,e,f,g]) if(hours === null || hours === undefined || hours === '' || hours < 0 || hours > 24) {
+        return 'one or more of the hours input are incorrect, please try again'
+    }
 
-    for(let day of daysOfWeek) hoursSlept.push(getSleepHours(day))
-    //sum up the hours
-    const totalHoursSlept = hoursSlept.reduce(
-        (day, nextDay) => day + nextDay 
-    )
-    return totalHoursSlept
+    return a + b + c + d + e + f + g
 }
 
 //get the ideal sleep hours that you prefer
