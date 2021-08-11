ed26a5b228ba0a8378ddc67e26d161c1e182c271
Farai Tanekha <30661410+codemusltd@users.noreply.github.com>
2021-08-11 15:12:31 +0100

    final commit

--- sleepdebtCalculator.js
+++ sleepdebtCalculator.js
@@ -1,27 +1,5 @@
 'use strict'
 
-// //determine number of hours of sleep you got per night of the week
-// function getSleepHours(day){
-//   //make the function flexible
-//     day = day.toLowerCase()
-//     //return the number of hours you slept each night/day
-//     switch(day){
-//         case 'sunday': case'monday': 
-//         case 'tuesday':
-//             return 5.5
-//         case 'wednesday':
-//             return 6
-//         case 'thursday':
-//             return 7
-//         case 'friday': case 'saturday':
-//             return 8.5
-//         case 'sunday':
-//             return 8
-//         default: 
-//             return 'day unrecongnized!'
-//     }
-// }
-
 //get the total sleep hours that you actually slept
 function getActualSleepHours(a = 5.5, b = 5.5, c = 5.5, d = 6, e = 7, f = 8.5, g = 8){
     //check that hours are valid
